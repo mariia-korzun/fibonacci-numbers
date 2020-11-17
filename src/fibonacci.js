30d7c0b95faab168acf9232c07e6df27fc606898
Mariia Korzun <3151020m@gmail.com>
2020-11-17 20:12:26 +0200

    task 2 is done.

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacciNumbers = (num) => {
-    // Your implementation
-    // Read README.md file, if you not understand what to do
+    let number1 = 1
+    let number2 = 1
+    if (typeof num !== 'number') {
+        return 'Passed argument is not a number'
+    }
+    if(num <= 2) { return 1 }
+    return fib(number1, number2, num - 2)
 };
+
+function fib(number1, number2, num) {
+    num--
+    let newNumber1 = number2
+    number2 = number1 + number2
+    if (num==0) {
+        return number2
+    }
+    return fib(newNumber1, number2, num)
+}
+
 module.exports = fibonacciNumbers;
